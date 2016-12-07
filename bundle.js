264dbf98ae87996ddf8c9028638f6bedd01ecda0
pridemusvaire <musvairepridetafadzwa@gmail.com>
2016-12-07 08:06:24 +0200

    'add'

--- bundle.js
+++ bundle.js
@@ -27822,7 +27822,7 @@
 	                        _react2.default.createElement(
 	                            "p",
 	                            { className: "icons" },
-	                            _react2.default.createElement("a", { href: "#", className: "insights" })
+	                            _react2.default.createElement("a", { href: "#", className: this.props.course.CourseType })
 	                        )
 	                    )
 	                )
